a0f7ae8b40adb593c928c947bdbfb305ce371796
Moisés Pellegrin <104878641+MoisaAP@users.noreply.github.com>
2023-03-04 00:28:50 -0300

    Maior-valor.c

--- Maior-valor.c
+++ Maior-valor.c
@@ -1,8 +1,8 @@
-/*  3 Construa uma função que receba como parâmetro um vetor de inteiros com 10 valores aleatórios
-entre 0 e 6, preenchidos automaticamente. A função deve determinar o maior elemento e o número
-de vezes que ele ocorre no vetor. Por exemplo, para o vetor [1, 3, 5, 5, 2, 4, 5, 3, 1, 0] a função deve
-determinar o valor 5 e indicar que ele ocorreu 3 vezes. A função deve ser do tipo void e o programa
-deve, após a execução da função, listar o vetor e apresentar os valores manipulados por ela (maior
+/* Construa uma funÃ§Ã£o que receba como parÃ¢metro um vetor de inteiros com 10 valores aleatÃ³rios
+entre 0 e 6, preenchidos automaticamente. A funÃ§Ã£o deve determinar o maior elemento e o nÃºmero
+de vezes que ele ocorre no vetor. Por exemplo, para o vetor [1, 3, 5, 5, 2, 4, 5, 3, 1, 0] a funÃ§Ã£o deve
+determinar o valor 5 e indicar que ele ocorreu 3 vezes. A funÃ§Ã£o deve ser do tipo void e o programa
+deve, apÃ³s a execuÃ§Ã£o da funÃ§Ã£o, listar o vetor e apresentar os valores manipulados por ela (maior
 valor e quantidade de vezes que aparece no vetor). */
 
 #include <stdio.h>
